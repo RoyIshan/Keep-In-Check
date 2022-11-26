fac52e90db51476ae17f87a6ab08f51dcbe62d5e
darshanpatel <pdarshan893@gmail.com>
2022-11-26 11:16:13 +0530

    Worker Location is saved on firebase

--- app/src/main/java/com/trystar/keepincheck/MainActivity.java
+++ app/src/main/java/com/trystar/keepincheck/MainActivity.java
@@ -13,19 +13,22 @@ import com.google.firebase.auth.FirebaseAuth;
 public class MainActivity extends AppCompatActivity {
 
     Button signOut;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         signOut = findViewById(R.id.signOut);
         signOut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 FirebaseAuth.getInstance().signOut();
-                startActivity(new Intent(MainActivity.this,SelectIdentity.class));
-                Toast.makeText(MainActivity.this,"Signing Out",Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(MainActivity.this, SelectIdentity.class));
+                Toast.makeText(MainActivity.this, "Signing Out", Toast.LENGTH_SHORT).show();
             }
         });
     }
 }
+
