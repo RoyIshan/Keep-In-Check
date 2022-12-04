c1e6ebb8082bb01e6a73f894e5b1a2cc0dc63415
Ishan Roy <ishanroy99@yahoo.com>
2022-12-04 13:14:01 +0530

    NOTHING

--- app/src/main/java/com/trystar/keepincheck/AssignTask.java
+++ app/src/main/java/com/trystar/keepincheck/AssignTask.java
@@ -1,13 +1,12 @@
 package com.trystar.keepincheck;
 
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import android.os.Bundle;
-import android.view.View;
-
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.firestore.DocumentChange;
 import com.google.firebase.firestore.EventListener;
@@ -16,16 +15,12 @@ import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.trystar.keepincheck.Adapter.ToDoAdapter;
 import com.trystar.keepincheck.Model.ToDoModel;
-
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
 public class AssignTask extends AppCompatActivity {
 
-    private RecyclerView recyclerView;
-    private FloatingActionButton wFab;
     private FirebaseFirestore firestore;
     private ToDoAdapter adapter;
     private List<ToDoModel> mList;
@@ -35,8 +30,8 @@ public class AssignTask extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_assign_task);
 
-        recyclerView = findViewById(R.id.recyclerView);
-        wFab  = findViewById(R.id.assigntask);
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        FloatingActionButton wFab = findViewById(R.id.assigntask);
         firestore = FirebaseFirestore.getInstance();
 
         recyclerView.setHasFixedSize(true);
@@ -58,6 +53,7 @@ public class AssignTask extends AppCompatActivity {
 
     private void showData(){
         firestore.collection("task").addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @SuppressLint("NotifyDataSetChanged")
             @Override
             public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
                 for (DocumentChange documentChange: value.getDocumentChanges()){
