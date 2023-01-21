9f96198e83d176e735b167f82f9b5846879dc815
Ishan Roy <ishanroy99@yahoo.com>
2023-01-21 15:16:11 +0530

    Minor Update

--- app/src/main/java/com/trystar/keepincheck/SelectIdentity.java
+++ app/src/main/java/com/trystar/keepincheck/SelectIdentity.java
@@ -2,7 +2,6 @@ package com.trystar.keepincheck;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -23,28 +22,22 @@ public class SelectIdentity extends AppCompatActivity {
         worker = findViewById(R.id.Worker);
 
         try {
-            owner.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    try {
-                        startActivity(new Intent(SelectIdentity.this, OwnerRegister.class));
-                    }
-                    catch (Exception e)
-                    {
-                        Toast.makeText(SelectIdentity.this, e.getMessage().toString(), Toast.LENGTH_SHORT).show();
-                    }
+            owner.setOnClickListener(view -> {
+                try {
+                    startActivity(new Intent(SelectIdentity.this, OwnerRegister.class));
+                }
+                catch (Exception e)
+                {
+                    Toast.makeText(SelectIdentity.this, e.getMessage().toString(), Toast.LENGTH_SHORT).show();
                 }
             });
-            worker.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    try {
-                        startActivity(new Intent(SelectIdentity.this, Register.class));
-                    }
-                    catch (Exception e)
-                    {
-                        Toast.makeText(SelectIdentity.this, e.getMessage().toString(), Toast.LENGTH_SHORT).show();
-                    }
+            worker.setOnClickListener(view -> {
+                try {
+                    startActivity(new Intent(SelectIdentity.this, Register.class));
+                }
+                catch (Exception e)
+                {
+                    Toast.makeText(SelectIdentity.this, e.getMessage().toString(), Toast.LENGTH_SHORT).show();
                 }
             });
         }
