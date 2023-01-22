040fd8edb2e902a7f7b443aac44bf24390aa75fa
Darshan Patel <70792411+han-x09@users.noreply.github.com>
2023-01-22 22:01:40 +0530

    Map activity with random locations

---
+++ app/src/main/java/com/trystar/keepincheck/mapfiles/ViewPagerAdapter.java
@@ -0,0 +1,34 @@
+package com.trystar.keepincheck.mapfiles;
+
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+
+    List<Location> list ;
+    List<LocationFragment> fragmentList = new ArrayList<>();
+
+    public ViewPagerAdapter(FragmentManager fm, List<Location> list) {
+        super(fm);
+        this.list = list;
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        LocationFragment fragment = LocationFragment.newInstance(list.get(i));
+        fragmentList.add(fragment);
+        return  fragment;
+    }
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+
+}
